559d238942a5ecd291a498f7db323eb7e03091fc
Pug <iakingdoms@gmail.com>
2022-12-19 16:29:26 +1100

    added basic start screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+import StartScreen from "./components/StartScreen";
+
 function App() {
+	const [hasStarted, setHasStarted] = useState(false);
+
 	return (
-		<div>
-			<p>App</p>
+		<div className="absolute h-screen w-screen bg-blue-400">
+			<div className="flex justify-center mt-96">
+				{!hasStarted && <StartScreen setHasStarted={setHasStarted} />}
+			</div>
 		</div>
 	);
 }
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,18 @@
+function StartScreen(props) {
+	return (
+		<div className="flex flex-col gap-5">
+			<p className="text-white font-bold text-6xl stroke-red-500">HangMan</p>
+			<button
+				onClick={() => props.setHasStarted(true)}
+				className="bg-blue-500 rounded-lg text-white h-10 hover:bg-blue-600 active:bg-blue-800"
+			>
+				Start
+			</button>
+			<button className="bg-gray-500 rounded-lg text-white h-10 hover:bg-gray-600 active:bg-gray-800">
+				Rules
+			</button>
+		</div>
+	);
+}
+
+export default StartScreen;
